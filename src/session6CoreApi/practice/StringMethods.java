6f3484f51b5e4eb60d2c2088de7f34cd78ccb620
Dan <inb3zna@gmail.com>
2023-09-19 21:13:56 +0300

    practice from the session

---
+++ src/session6CoreApi/practice/StringMethods.java
@@ -0,0 +1,41 @@
+package session6CoreApi.practice;
+
+import java.io.FilterOutputStream;
+
+public class StringMethods {
+
+    public static void main(String[] args) {
+        String name = "Jane Swallow";
+        printStringLength("Cow");
+        printCharAtIndex("Alice", 1);
+        printStringIndexAt("Alice", "i");
+        printSubstring("Hello World", 6, 11);
+
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+
+        System.out.println(name + " contains Swallow: " + name.contains("Swallow"));
+        System.out.println("Replace 'e' from Hello: " + "Hello".replace('e', 'x'));
+
+        String email = "user@gmail.com";
+        String domain = email.substring(email.indexOf('@') + 1);
+        System.out.println(domain);
+
+    }
+
+    private static void printStringLength(String string){
+        System.out.println("String " + string + " has length of: " + string.length());
+    }
+
+    private static void printCharAtIndex(String string, int index){
+        System.out.println("The char at index " + index + " is: " + string.charAt(index));
+    }
+
+    private static void printStringIndexAt(String string, String target){
+        System.out.println("The index of target is: " + string.indexOf(target));
+    }
+
+    private static void printSubstring(String string, int startIndex, int endIndex){
+        System.out.println("Substring is " + string.substring(startIndex, endIndex));
+    }
+}
