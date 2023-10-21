c9380dffc51a5a9a06634dba9d17cc57fecbac7c
Dan <inb3zna@gmail.com>
2023-10-21 22:42:56 +0300

    playing with xml to json conversion

---
+++ src/session15_equals_hashcode/challenges/xml_to_json_conversion/Vehicle.java
@@ -0,0 +1,32 @@
+package session15_equals_hashcode.challenges.xml_to_json_conversion;
+
+public class Vehicle {
+
+    private String name;
+    private String color;
+    private String make;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+}
---
+++ src/session15_equals_hashcode/challenges/xml_to_json_conversion/readme.md
@@ -0,0 +1,7 @@
+### For jackson
+- weird error: 
+- Exception in thread "main" com.fasterxml.jackson.core.JsonParseException: ParseError at [row,col]:[1,1]
+  Message: Content is not allowed in prolog.
+
+### for ORG.Json
+- somehow format it
